87379904d83a01cfa8b63fc659e275c46002762b
Kamaa <kkamadi567@gmail.com>
2026-01-05 19:54:53 +0300

    React | Tailwind config files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+// j
+
+const App = () => {
+  return (
+    <>
+    <div className=' pt-4 items-center flex'> To do List</div>
+    <input className='bg-gray-800:' placeholder='input your to do'>
+    <button> </button>
+    </input>
+    </>
+  )
+}
+
+export default App
