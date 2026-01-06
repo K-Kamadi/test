10c3be5b0a8bc2c4699d34ddde9f4c89cf0c95bc
Kamaa <kkamadi567@gmail.com>
2026-01-06 14:22:43 +0300

    Component-Todoitems

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,26 +1,35 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import todo_icon from '../assets/todo_icon.png'
+import Todoitems from '../components/Todoitems'
 
 const Todo = () => {
+
+    {/* text input func */}
+    const inputRef =useRef ()
+    const add=()=>{
+        const inputText = inputRef.current.value
+    }
+
   return (
     <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
-    {/* Title */}
-    <div className='flex items-center mt-7 gap-2'>
-        <img className='w-8' src={todo_icon} alt="" />
-        <h1 className='text-3xl font-semibold '>To do list</h1>
-    </div>
+        {/* Title */}
+        <div className='flex items-center mt-7 gap-2'>
+            <img className='w-8' src={todo_icon} alt="" />
+            <h1 className='text-3xl font-semibold '>To do list</h1>
+        </div>
 
-    {/* input box */}
-    <div className='flex items-center my-7 bg-gray-200 rounded-full'>
-        <input className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Add your task' />
-        <button className='border-none rounded-full bg-orange-600 w-32 h-14
-         text-white text-lg font-medium cursor-pointer'>Add +</button>
-    </div>
+        {/* input box */}
+        <div className='flex items-center my-7 bg-gray-200 rounded-full'>
+            <input className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600'
+             type="text" placeholder='Add your task' ref={inputRef} />
+            <button className='border-none rounded-full bg-orange-600 w-32 h-14
+            text-white text-lg font-medium cursor-pointer'>Add +</button>
+        </div>
 
-    {/* todo list */}
-    <div>
-        
-    </div>
+        {/* todo list */}
+        <div>
+        <Todoitems text='leare'/>   
+        </div>
 
     </div>
   )
